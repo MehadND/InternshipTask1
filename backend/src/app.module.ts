54067167e030f11f9e5bd0e7df105e40a3d858f0
MehadND <mehadnd@gmail.com>
2024-06-08 18:08:19 +0500

    feat(backend): imported Moongose module into app and todo modules to allow the usage of moongose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TodoModule, AuthModule],
+  imports: [
+    TodoModule,
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/TDC'),
+  ],
   controllers: [],
   providers: [],
 })

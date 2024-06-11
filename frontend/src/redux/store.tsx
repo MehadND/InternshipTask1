af1b102866a180809ed204f26890fb967a270e82
MehadND <mehadnd@gmail.com>
2024-06-11 14:06:23 +0500

    chore(frontend): added all the newly created slices in redux store

--- frontend/src/redux/store.tsx
+++ frontend/src/redux/store.tsx
@@ -1,10 +1,18 @@
 // store.ts
 import { configureStore } from "@reduxjs/toolkit";
 import todoSlice from "./features/todo/todoSlice";
+import selectedTaskSlice from "./features/selectedTask/selectedTaskSlice";
+import sheetOpenSlice from "./features/sheetOpen/sheetOpenSlice";
+import paginationSlice from "./features/pagination/paginationSlice";
+import authSlice from "./features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     todos: todoSlice,
+    selectedTask: selectedTaskSlice,
+    sheetOpen: sheetOpenSlice,
+    pagination: paginationSlice,
+    auth: authSlice,
   },
 });
 

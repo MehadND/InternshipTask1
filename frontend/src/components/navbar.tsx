955a9f5067235603d86982cae1db0ececcaad020
MehadND <mehadnd@gmail.com>
2024-06-11 14:07:44 +0500

    feat(frontend): created a UI component for handling Login and Logout user
    added Login Component into navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 import { LightModeToggle } from "./mode-toggle";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
+import Login from "./login";
 
 const Navbar = () => {
   return (
@@ -9,7 +14,23 @@ const Navbar = () => {
         </h1>
       </div>
 
-      <LightModeToggle />
+      <div className="flex items-center gap-4">
+        <HoverCard>
+          <HoverCardTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>
+                <span className="text-xs">Username</span>
+              </AvatarFallback>
+            </Avatar>
+          </HoverCardTrigger>
+          <HoverCardContent className="m-4">
+            <Login />
+          </HoverCardContent>
+        </HoverCard>
+
+        <LightModeToggle />
+      </div>
     </div>
   );
 };

6084f021ab1e6082e5ab0aa111517370105b48ed
MehadND <mehadnd@gmail.com>
2024-06-11 14:09:03 +0500

    chore(frontend): modified App.tsx to include all the components required for rendering
    such as:
    - displaying todos
    - showing pagination controls
    - a component for creating task

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,54 @@
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "./components/navbar";
+import Pagination from "./components/pagination";
+import TodoDetails from "./components/todo-details";
+import TodosUI from "./components/todo-ui";
+import { Separator } from "./components/ui/separator";
+import { AppDispatch, RootState } from "./redux/store";
+import { useEffect, useState } from "react";
+import { fetchTodos } from "./redux/features/todo/todoSlice";
+import CreateTask from "./components/create-task";
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { itemsPerPage, currentPage } = useSelector(
+    (state: RootState) => state.pagination
+  );
+  const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+
+  useEffect(() => {
+    dispatch(fetchTodos({ itemsPerPage: itemsPerPage, page: currentPage }));
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, [itemsPerPage, currentPage, dispatch]);
+
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <div className="font-notosans">
+    <div className="">
       <Navbar />
+
+      <div className="p-4 justify-center flex items-center">
+        {isLoading ? (
+          <div className="flex h-96 items-center justify-center">
+            <span className="loader"></span>
+          </div>
+        ) : (
+          <div className="w-full max-w-screen-md">
+            <TodosUI />
+
+            <TodoDetails />
+
+            <Separator />
+
+            <Pagination />
+          </div>
+        )}
+      </div>
+
+      {!isLoading && <CreateTask />}
     </div>
   );
 }

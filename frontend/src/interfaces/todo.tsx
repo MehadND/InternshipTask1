be6573f8d9a2fe325a0b043f3edac5f1306e1b33
MehadND <mehadnd@gmail.com>
2024-06-24 15:20:04 +0500

    chore(frontend): updated todo interface according to the new additions in Task such as due date and subtasks

--- frontend/src/interfaces/todo.tsx
+++ frontend/src/interfaces/todo.tsx
@@ -1,7 +1,15 @@
+export interface Subtask {
+  title: string;
+  isComplete?: boolean;
+}
+
 export interface Todo {
   _id?: string;
   taskTitle: string;
   taskDescription?: string;
   isComplete?: boolean;
   createdAt?: Date;
+  updatedAt?: Date;
+  dueDate?: Date;
+  subtasks?: Subtask[];
 }

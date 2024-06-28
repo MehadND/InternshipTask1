df72a471b2118fa20a522193d09ceb5160cb1c02
MehadND <mehadnd@gmail.com>
2024-06-28 11:33:07 +0500

    chore(backend): updated custom exception filter
    Handle Throttle Exception

--- backend/src/todo/filters/http-exception.filter.ts
+++ backend/src/todo/filters/http-exception.filter.ts
@@ -3,20 +3,35 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
+import { ThrottlerException } from '@nestjs/throttler';
 import { Request, Response } from 'express';
 
-@Catch(HttpException)
+@Catch(HttpException, ThrottlerException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: HttpException | ThrottlerException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
+    if (
+      status === HttpStatus.TOO_MANY_REQUESTS &&
+      request.originalUrl === '/api/todo/generatedescription'
+    ) {
+      response.status(status).json({
+        statusCode: status,
+        message: 'Over using the Generative AI API endpoint',
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+      return;
+    }
+
     response.status(status).json({
       statusCode: status,
-      message: exception.message,
+      message: exception.message || null,
       timestamp: new Date().toISOString(),
       path: request.url,
     });

bdd49b6e6d90b0e9d4ec4d2164f0717b61e74655
MehadND <mehadnd@gmail.com>
2024-06-28 11:34:05 +0500

    chore(backend): integrated middleware and throttle into app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,40 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { LoggerMiddleware } from './todo/middleware/logger.middleware';
 
 @Module({
   imports: [
     TodoModule,
     AuthModule,
     MongooseModule.forRoot('mongodb://localhost:27017/TDC'),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}

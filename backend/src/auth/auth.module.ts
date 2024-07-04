caf3c3bdce2cd945865fd93e675a5996424a3a94
MehadND <mehadnd@gmail.com>
2024-07-04 10:50:21 +0500

    chore(backend): cleanup + formatiing of auth folder

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -13,10 +13,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      // useFactory: (configService: ConfigService) => ({
-      //   secret: configService.get('JWT_SECRET_KEY'),
-      //   signOptions: { expiresIn: '7d' },
-      // }),
       useFactory: async (configService: ConfigService) => {
         return {
           secret: 'JWT_SECRET_KEY',
--- backend/src/auth/strategies/local.strategy.ts
+++ backend/src/auth/strategies/local.strategy.ts
@@ -1,7 +1,7 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -10,8 +10,8 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   validate(username: string, password: string) {
-    // const user = this.authService.login({ username, password });
-    // if (!user) throw new UnauthorizedException();
+    const user = this.authService.login({ username, password });
+    if (!user) throw new UnauthorizedException();
 
     return { username, password };
   }
